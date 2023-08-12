517f9f2817b4e4a484bf7a4cd31d4f5d58cf76a4
pedro perkoso <pedroperkoso@gmail.com>
2023-08-12 19:27:45 +0700

    push

---
+++ connection/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+$db = "restounikom_db";
+$conn = mysqli_connect($host, $user, $pass, $db);
+mysqli_select_db($conn, $db);
+
+if (!$conn) {
+	die('Maaf koneksi gagal: ' . $connect->connect_error);
+} else {
+	//echo 'Tahu';
+	//echo $_SESSION['viewnya'];
+}
+?>
